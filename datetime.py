130acfd249e8cb1c173e0a874501f1c6aa9c7e93
ag143 <ag14341@gmail.com>
2022-04-25 10:44:08 +0530

    Update datetime.py

--- datetime.py
+++ datetime.py
@@ -1,24 +1,24 @@
-from datetime import datetime 
+from datetime import _datetime 
 
 x = datetime.datetime.now()
 
 print(x)
 print("#####################")
-from datetime import datetime
+from datetime import _datetime
 
 x = datetime.datetime.now()
 
 print(x.year)
 print(x.strftime("%A"))
 print("#####################")
-from datetime import datetime
+from datetime import _datetime
 
 x = datetime.datetime(2020, 5, 17)
 
 print(x)
 
 print("#####################")
-from datetime import datetime
+from datetime import _datetime
 
 x = datetime.datetime(2018, 6, 1)
 
