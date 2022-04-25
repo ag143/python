3f2ead9ee850b1ac1ccd59e8babd2cfa39883793
ag143 <ag14341@gmail.com>
2022-04-25 21:55:00 +0530

    Update datetimetest.py

--- datetimetest.py
+++ datetimetest.py
@@ -5,23 +5,23 @@ x = datetime.now()
 
 print(x)
 print("#####################")
-from datetime import _datetime
 
-x = datetime.datetime.now()
+
+x = datetime.now()
 
 print(x.year)
 print(x.strftime("%A"))
 print("#####################")
-from datetime import _datetime
 
-x = datetime.datetime(2020, 5, 17)
+
+x = datetime(2020, 5, 17)
 
 print(x)
 
 print("#####################")
 from datetime import _datetime
 
-x = datetime.datetime(2018, 6, 1)
+x = datetime(2018, 6, 1)
 
 print(x.strftime("%B"))
 print("#####################")
